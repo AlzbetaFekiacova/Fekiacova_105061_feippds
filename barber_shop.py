a05ba03975fab7ebae500fd7c70e268a8840f7e7
Alžbeta Fekiačová <xfekiacova@stuba.sk>
2023-03-04 12:36:11 +0100

    feat: implement barber and customer function

--- barber_shop.py
+++ barber_shop.py
@@ -14,6 +14,9 @@ from fei.ppds import Mutex, Thread, Semaphore, print
 from time import sleep
 from random import randint
 
+C: int = 5
+N: int = 3
+
 
 class Shared(object):
 
@@ -26,32 +29,38 @@ class Shared(object):
         # self.barber = Rendezvous is implemented as ?
         # self.customer_done = Rendezvous is implemented as ?
         # self.barber_done = Rendezvous is implemented as ?
+        self.customer = Semaphore(0)
+        self.barber = Semaphore(0)
+        self.customer_done = Semaphore(0)
+        self.barber_done = Semaphore(0)
 
 
 def get_haircut(i):
     # TODO: Simulate time and print info when customer gets haircut
     fei.ppds.print(f'Customer {i} is getting his hair cut.')
-    sleep(1)
+    sleep(3)
 
 
 def cut_hair():
-# TODO: Simulate time and print info when barber cuts customer's hair
+    # TODO: Simulate time and print info when barber cuts customer's hair
     fei.ppds.print(f'Barber is cutting hair')
-    sleep(1)
+    sleep(4)
 
 
 def balk(i):
-# TODO: Represents situation when waiting room is full and print info
+    # TODO: Represents situation when waiting room is full and print info
     fei.ppds.print(f'Customer {i} cannot enter the waiting room, room is full.')
-    sleep(2)
+    sleep(5)
 
 
 def growing_hair(i):
-# TODO: Represents situation when customer wait after getting haircut. So hair is growing and customer is sleeping for some time
+    # TODO: Represents situation when customer wait after getting haircut. So hair is growing and customer is sleeping for some time
     fei.ppds.print(f'Customer {i} waits, hair is growing.')
-    sleep(3)
+    sleep(8)
+
 
 def customer(i, shared):
+    global N
     # TODO: Function represents customers behaviour. Customer come to waiting if room is full sleep.
     # TODO: Wake up barber and waits for invitation from barber. Then gets new haircut.
     # TODO: After it both wait to complete their work. At the end waits to hair grow again
@@ -59,23 +68,49 @@ def customer(i, shared):
     while True:
         # TODO: Access to waiting room. Could customer enter or must wait? Be careful about counter integrity :)
 
+        shared.mutex.lock()
+        fei.ppds.print(f'Customer {i} entered the waiting room')
+        if shared.waiting_room == N:
+            balk(i)
+            shared.mutex.unlock()
+            continue
+
+        shared.waiting_room += 1
+        shared.mutex.unlock()
+
+        shared.customer.signal()
+        shared.barber.wait()
         # TODO: Rendezvous 1
         get_haircut(i)
         # TODO: Rendezvous 2
-
+        shared.customer_done.signal()
+        shared.barber_done.wait()
         # TODO: Leave waiting room. Integrity again
+
+        shared.mutex.lock()
+        shared.waiting_room -= 1
+        shared.mutex.unlock()
         growing_hair(i)
 
 
 def barber(shared):
-    # TODO: Function barber represents barber. Barber is sleeping.
+    # TODO: Function barber repres
+    #  ents barber. Barber is sleeping.
     # TODO: When customer come to get new hair wakes up barber.
     # TODO: Barber cuts customer hair and both wait to complete their work.
 
     while True:
+        shared.customer.wait()
+        shared.barber.signal()
         # TODO: Rendezvous 1
+
         cut_hair()
         # TODO: Rendezvous 2
+        shared.customer_done.wait()
+        shared.barber_done.signal()
+
+        fei.ppds.print(f'Barber is done cutting hair')
+
 
 
 def main():
