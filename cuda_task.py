e2c2a911f5a432eaa2dbade309713f460efad082
Alžbeta Fekiačová <xfekiacova@stuba.sk>
2023-05-11 11:54:11 +0200

    refactor: extract conversion to separate function

--- cuda_task.py
+++ cuda_task.py
@@ -10,53 +10,52 @@ from timeit import default_timer as timer
 from numba import cuda
 
 
+def convert_image(image, x, y):
+    r = image[x, y, 0]
+    g = image[x, y, 1]
+    b = image[x, y, 2]
+    return 0.299 * r + 0.587 * g + 0.114 * b
+
+
 def rgb_to_gray(input_image):
     output_image = np.zeros((input_image.shape[0], input_image.shape[1], input_image.shape[2]),
                             dtype=np.float32)
     for x in range(len(input_image)):
         for y in range(len(input_image[x])):
-            r = input_image[x, y, 0]
-            g = input_image[x, y, 1]
-            b = input_image[x, y, 2]
-            gray = 0.299 * r + 0.587 * g + 0.114 * b
-            output_image[x, y] = gray
+            output_image[x, y] = convert_image(input_image, x, y)
     return output_image
 
 
-def cpu_func(image_path):
+def cpu_func(image_path, cpu_time):
     input_pixels = plt.imread(image_path)
     start = timer()
     converted_image = rgb_to_gray(input_pixels)
     output_img = np.clip(converted_image, 0, 255).astype(np.uint8)
     end = timer()
     print(f"-CPU- Conversion to grayscale took : {end - start} seconds.")
-
-    image_path = image_path.replace("original", "transformed_CPU")
+    cpu_time += (end - start)
+    image_path = image_path.replace("copied", "sports_transformed_CPU")
     image_path_split = image_path.split(".")
     image_path_split[0] = image_path_split[0] + "_grayscale_CPU"
     image_path_output = ".".join(image_path_split)
     plt.imsave(image_path_output, output_img, format="jpg")
 
 
-def cpu_main():
-    basic_path = "images/original/flower_s"
-    for i in range(20):
+def cpu_main(cpu_time):
+    basic_path = "images/copied/image_"
+    for i in range(100):
         current_path = basic_path + str(i) + ".jpg"
-        cpu_func(current_path)
+        cpu_func(current_path, cpu_time)
 
 
 @cuda.jit
 def rgb_to_gray_cuda(input_image, output_image):
     x, y = cuda.grid(2)
     if x < input_image.shape[0] and y < input_image.shape[1]:
-        r = input_image[x, y, 0]
-        g = input_image[x, y, 1]
-        b = input_image[x, y, 2]
-        gray = 0.299 * r + 0.587 * g + 0.114 * b
-        output_image[x, y] = gray
+        output_image[x, y] = convert_image(input_image, x, y)
 
 
-def cuda_func(image_path):
+def cuda_func(image_path, gpu_time):
     image = plt.imread(image_path)
     height = image.shape[0]
     weight = image.shape[1]
@@ -75,22 +74,29 @@ def cuda_func(image_path):
     rgb_to_gray_cuda[blocks_per_grid, threads_per_block](d_input_image, d_output_image)
     end = timer()
     print(f'-GPU- Conversion to grayscale took {end - start} seconds')
+    gpu_time += (end - start)
 
     output_img = np.clip(d_output_image.copy_to_host(), 0, 255).astype(np.uint8)
 
-    image_path = image_path.replace("original", "transformed_GPU")
+    image_path = image_path.replace("copied", "sports_transformed_GPU")
     image_path_split = image_path.split(".")
     image_path_split[0] = image_path_split[0] + "_grayscale_GPU"
     image_path_output = ".".join(image_path_split)
     plt.imsave(image_path_output, output_img, format="jpg")
 
 
-def gpu_main():
-    basic_path = "images/original/flower_s"
-    for i in range(20):
+def gpu_main(gpu_time):
+    basic_path = "images/copied/image_"
+    for i in range(100):
         current_path = basic_path + str(i) + ".jpg"
-        cuda_func(current_path)
+        cuda_func(current_path, gpu_time)
 
 
 if __name__ == '__main__':
-    cpu_main()
+    cpu_time = 0
+    gpu_time = 0
+    cpu_main(cpu_time)
+    gpu_main(gpu_time)
+
+    print(f'Average conversion time on CPU took -{cpu_time/100}-')
+    print(f'Average conversion time on GPU took -{gpu_time/100}-')
